b9d3329ed3fae76aa445af0395c6a46f0943f76d
anassbzd <45536197+anassbzd@users.noreply.github.com>
2019-05-24 14:14:28 +0200

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "myrobot.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+static MyRobot Robot;
+void MainWindow::connect()
+{
+  Robot.doConnect();
+}
